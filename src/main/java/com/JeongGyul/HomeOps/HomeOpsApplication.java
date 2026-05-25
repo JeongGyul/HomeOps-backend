9e4587aae2e8368b5e988488074348536da5daa2
JeongGyul <jjascd@naver.com>
2026-05-26 01:00:19 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/JeongGyul/HomeOps/HomeOpsApplication.java
@@ -0,0 +1,13 @@
+package com.JeongGyul.HomeOps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeOpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JeongGyul/HomeOps/HomeOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JeongGyul.HomeOps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
