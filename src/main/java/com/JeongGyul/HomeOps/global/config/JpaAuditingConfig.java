0dc0ce2747f38240544e2180cd9c365c4b13699b
JeongGyul <jjascd@naver.com>
2026-06-03 01:26:08 +0900

    chore: BaseEntity 및 관련 설정 추가

---
+++ src/main/java/com/JeongGyul/HomeOps/global/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.JeongGyul.HomeOps.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
