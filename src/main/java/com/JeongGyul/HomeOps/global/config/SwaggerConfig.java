9b8a4eb4f8e4378b87a5d77f7f666257205679e1
JeongGyul <jjascd@naver.com>
2026-06-09 23:48:43 +0900

    chore: Swagger 설정

---
+++ src/main/java/com/JeongGyul/HomeOps/global/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.JeongGyul.HomeOps.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI swagger() {
+        Info info = new Info().
+                title("HomeOps API").
+                description("HomeOps의 API 문서입니다.").
+                version("1.0.0");
+
+        String securityScheme = "JWT";
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList(securityScheme);
+
+        Components components = new Components()
+                .addSecuritySchemes(
+                        securityScheme,
+                        new SecurityScheme()
+                                .name(securityScheme)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                );
+
+        return new OpenAPI()
+                .info(info)
+                .addServersItem(new Server().url("/"))
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
